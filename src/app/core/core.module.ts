e784cebcbe53b878612617bcd1e4ff3cc3a4ffd2
Antía <elyosis@proton.me>
2023-11-02 00:15:33 +0100

    Created header and navigation components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    NavigationComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    NavigationComponent
   ]
 })
 export class CoreModule { }
