b96e1fac615e0f8927cab2230607da4a4279bd70
Antía <elyosis@proton.me>
2023-11-05 17:32:04 +0100

    Created and imported Twimp card component

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+
 import { DashboardComponent } from './dashboard.component';
 
 
@@ -9,7 +11,8 @@ import { DashboardComponent } from './dashboard.component';
     DashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [DashboardComponent]
 })
