a5be17d550249ddc05e07e126a03b3f5a406e79b
Antía <elyosis@proton.me>
2023-11-18 17:30:43 +0100

    Added author-card component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TwimpCardComponent } from './twimp/twimp-card/twimp-card.component';
 import { TwimpListComponent } from './twimp/twimp-list/twimp-list.component';
+import { AuthorCardComponent } from './author/author-card/author-card.component';
 
 
 
 @NgModule({
   declarations: [
     TwimpCardComponent,
-    TwimpListComponent
+    TwimpListComponent,
+    AuthorCardComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [TwimpCardComponent, TwimpListComponent]
+  exports: [TwimpCardComponent, TwimpListComponent, AuthorCardComponent]
 })
 export class SharedModule { }
