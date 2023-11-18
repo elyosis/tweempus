d6a402e50101b46539676b88adb17ceb7978e1b8
Antía <elyosis@proton.me>
2023-11-18 18:03:21 +0100

    Added children routes to Profile module

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { Router, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile.component';
 import { ProfileRoutingModule } from './profile-routing.module';
+import { MyTwimpsComponent } from './my-twimps/my-twimps.component';
+import { FavoriteTwimpsComponent } from './favorite-twimps/favorite-twimps.component';
 
 
 
 @NgModule({
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    MyTwimpsComponent,
+    FavoriteTwimpsComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    RouterModule
   ]
 })
 export class ProfileModule { }
