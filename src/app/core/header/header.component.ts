c7acb1461cc271277b877b52f9a66efbe12afe30
Antía <elyosis@proton.me>
2023-11-23 22:03:53 +0100

    Added logout handler to Header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'tweempus-header',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private authService: AuthenticationService) {
+
+  }
+
+  checkLoginStatus() {
+    if (this.authService.token != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logOut() {
+    this.authService.logout()
+  }
 }
