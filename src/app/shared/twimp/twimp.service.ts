1590d0bd59a75881aecce0b24799b3b4121aac64
Antía <elyosis@proton.me>
2023-11-23 18:29:37 +0100

    Implemented GET methods on TwimpService

--- src/app/shared/twimp/twimp.service.ts
+++ src/app/shared/twimp/twimp.service.ts
@@ -1,11 +1,54 @@
-import { Injectable } from '@angular/core';
+import { Injectable} from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
 import { Observable } from 'rxjs';
-import { map, catchError } from 'rxjs';
+import { map, catchError, throwError } from 'rxjs';
+
+import { Author } from '../author/author.model';
+import { Twimp } from './twimp.model';
 
 @Injectable()
 export class TwimpService {
 
-  constructor() { }
+  url: string = "http://localhost:3000/twimps";
+  urlFavorite: string = "http://localhost:3000/author-favorites";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getTwimps(): Observable<Twimp[]> {
+    let twimps: Twimp[] = [];
+
+    return this.httpClient.get(this.url).pipe(
+      map((twimpsList: any) => {
+        for (let i in twimpsList) {
+          let twimp: Twimp = new Twimp(twimpsList[i].id, `http://localhost:4200/twimp/` + twimpsList[i].id, new Author(twimpsList[i].author), twimpsList[i].content, twimpsList[i].timestamp);
+          twimps.push(twimp);
+        }
+        return twimps;
+      }),
+      catchError(this.handleError))
+  }
+
+  getFavoritesByAuthor(idAuthor: string, idTwimp: string): Observable<boolean> {
+    return this.httpClient.get(`${this.urlFavorite}/${idAuthor}`).pipe(
+      map((response: any) => {
+        let favorites: string[] = response["twimps"];
+
+        if (favorites.indexOf(idTwimp) == -1) {
+          return false
+        } else {
+          return true
+        }
+      }),
+      catchError(this.handleError)
+    )
+  }
+
+  handleError(error: any) {
+    let errMsg = error.message ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : "Server error"
+
+    console.error(errMsg);
+    return throwError(() => errMsg)
+  }
 }
