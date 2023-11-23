bde476e4b10a922c26aa9a4ee82cd3c80fec2110
Antía <elyosis@proton.me>
2023-11-23 18:30:51 +0100

    Implemented logic to fetch Twimps data from db

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Twimp } from '../shared/twimp/twimp.model';
+import { TwimpService } from '../shared/twimp/twimp.service';
+import { from } from 'rxjs';
+import { AuthorService } from '../shared/author/author.service';
 
 @Component({
   selector: 'tweempus-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  twimps: Twimp[] = [];
+
+  constructor(private twimpService: TwimpService, private authorService: AuthorService) {}
+
+  ngOnInit() {
+    this.twimpService.getTwimps().subscribe(twimps => {
+      from(twimps).subscribe(twimp => {
+        this.authorService.getAuthor(twimp.author.id).subscribe(author => {
+          twimp.author = author;
+          this.twimpService.getFavoritesByAuthor("1", twimp.id).subscribe(favorite => {
+            twimp.favorite = favorite;
+            this.twimps.push(twimp)
+          })
+        })
+      })
+    })
+  }
 }
--- src/app/shared/twimp/twimp-list/twimp-list.component.ts
+++ src/app/shared/twimp/twimp-list/twimp-list.component.ts
@@ -1,22 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Twimp } from '../twimp.model';
-import { Author } from '../../author/author.model';
 
 @Component({
   selector: 'tweempus-twimp-list',
   templateUrl: './twimp-list.component.html',
   styleUrls: ['./twimp-list.component.css']
 })
-export class TwimpListComponent implements OnInit {
-  authors: Author[] = [];
-  twimps: Twimp[] = [];
-
-  ngOnInit() {
-    this.authors.push(new Author("01"));
-    for (let i = 1; i < 5; i++) {
-      this.twimps.push(new Twimp (i.toString(), "", this.authors[0], "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam efficitur sodales libero, sit amet posuere arcu consectetur ut. Nam volutpat ligula ac nunc consectetur vestibulum.", "08/04/2023"));
-    }
-  }
-
+export class TwimpListComponent {
+  @Input() twimps!:  Twimp[];
 }
