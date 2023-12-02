9ba7ef5c5321a286e58b7cdde978e5c8581c2f6b
Antía <elyosis@proton.me>
2023-12-02 19:28:16 +0100

    Implemented user login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../core/authentication.service';
+import { AuthorService } from '../shared/author/author.service';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'tweempus-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  constructor(private authService: AuthenticationService) {}
+  userForm!: FormGroup;
+  userUnregistered: boolean = false;
 
-  logIn() {
-    this.authService.login("1");
+  constructor(private authService: AuthenticationService, private authorService: AuthorService, private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      idAuthor: ["", Validators.required]
+    });
+  }
+  
+  logIn(form: any) {
+    if (this.userUnregistered) {
+      this.userUnregistered = false;
+    }
+
+    this.authorService.getAuthor(form.value.idAuthor).subscribe({
+      next: author => this.authService.login(form.value.idAuthor),
+      error: err => this.userUnregistered = true
+    });
   }
 }
